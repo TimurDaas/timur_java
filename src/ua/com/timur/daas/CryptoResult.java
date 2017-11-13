137c00475e93404cdd2c940b53c4d17671ef3d2c
тимур50 <timurkadaas@gmail.com>
2017-11-14 01:08:59 +0200

    Second commit(new branch)

--- src/ua/com/timur/daas/CryptoResult.java
+++ src/ua/com/timur/daas/CryptoResult.java
@@ -4,7 +4,9 @@ package ua.com.timur.daas;
  * Created by тимур50 on 14.11.2017.
  */
 public enum CryptoResult {
-    Sign_OK(1);
+    Sign_OK(1),
+    Sign_Invalid(2),
+    Sign_Corrupted(3);
 
     private final int value;
 
